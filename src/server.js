0b5d91f196049ac82c37f04a1ce7da500f276808
Stella Cai <stella.sijiacai@gmail.com>
2022-03-09 02:56:32 -0500

    process.env.port

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ app.use('/users', require('./users/users.controller'));
 app.use(errorHandler);
 
 // start server
-const port = process.env.NODEJS_DOCKER_PORT || 8080;
-server = app.listen(port, '0.0.0.0', () => console.log('Server listening on port ' + port));
+const port = process.env.PORT || 4000;
+server = app.listen(port, () => console.log('Server listening on port ' + port));
 
 module.exports = server;
