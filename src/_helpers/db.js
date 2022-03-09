706a5e5cf8d2e950dece85dc9907ded5b2ae76f3
Stella Cai <stella.sijiacai@gmail.com>
2022-03-09 10:02:08 -0500

    Revert "cleaned up env vars"
    This reverts commit d63c40e332425278ae82aedc089dd3f381212f85.

--- src/_helpers/db.js
+++ src/_helpers/db.js
@@ -9,14 +9,16 @@ initialize();
 
 async function initialize() {
     
+    const host = process.env.DOCKER ? process.env.MYSQLDB_DOCKER_HOST : process.env.MYSQLDB_LOCAL_HOST;
     // create db if it doesn't already exist
     const config = {
-        host: process.env.MYSQLDB_HOST,
-        port: process.env.MYSQLDB_PORT,
+        host: host,
+        port: process.env.MYSQLDB_LOCAL_PORT,
         user: process.env.MYSQLDB_USER,
-        password: process.env.MYSQLDB_PASSWORD,
+        password: process.env.MYSQLDB_ROOT_PASSWORD,
         database: process.env.MYSQLDB_DATABASE
     }
+    console.log('config :>> ', config);
     // const { host, port, user, password, database } = config.database;
     const connection = await mysql.createConnection(config);
     await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.MYSQLDB_DATABASE}\`;`);
@@ -28,8 +30,8 @@ async function initialize() {
         process.env.MYSQLDB_ROOT_PASSWORD, 
         { 
             dialect: 'mysql', 
-            host: process.env.MYSQLDB_HOST, 
-            port: process.env.MYSQLDB_PORT, 
+            host: host, 
+            port: process.env.MYSQLDB_LOCAL_PORT, 
             logging: false 
         }
     );
