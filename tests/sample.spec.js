45215915a9e26da20d2493500f73d3a172c7b848
Stella Cai <stella.sijiacai@gmail.com>
2022-04-04 00:41:28 -0400

    added org and register and auth tests

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -18,12 +18,55 @@ describe('Sample test suite', () => {
     expect(true).toEqual(true);
   });
 
-  it('tests / endpoint', async () => {
+  it('Test home endpoint', async () => {
     const response = await request(app).get('/');
     expect(response.statusCode).toBe(200);
     expect(response.body.message).toEqual(
       "Welcome to Konsensus' backend! Windsor is Julian's favourite student!"
     );
+  });
+});
+
+describe('Users test suite', () => {
+
+  let organizationId;
+
+  it('Create an organization', async () => {
+    const response = await request(app)
+      .post('/organization/create')
+      .send({
+        name: 'Facebook',
+      });
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('Get all organizations', async () => {
+    const response = await request(app).get('/organization');
+    expect(response.statusCode).toBe(200);
+    expect(response.body.length).toBeGreaterThan(0);
+    organizationId = response.body[0].id;
+  });
+
+  it('Create a user', async () => {
+    const response = await request(app)
+      .post('/user/register')
+      .send({
+        firstName: 'Windsor',
+        lastName: 'Huang',
+        email: 'windsoriscool@gmail.com',
+        password: '123456',
+        organizationId: organizationId});
+    expect(response.statusCode).toBe(200);
+  });
+
+  it('Login a user', async () => {
+    const response = await request(app)
+      .post('/user/authenticate')
+      .send({
+        email: 'windsoriscool@gmail.com',
+        password: '123456'
+      });
+    expect(response.statusCode).toBe(200);
   });
 });
 
